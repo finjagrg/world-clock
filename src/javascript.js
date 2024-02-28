543003f585851060de4d64a65bb8a6adc23703a6
finjagrg <finjagoergg@gmail.com>
2024-02-28 14:56:13 +0100

    added the current time

--- src/javascript.js
+++ src/javascript.js
@@ -21,8 +21,11 @@ function updateTime() {
 }
 function updateCity(event) {
   let cityTimezone = event.target.value;
-  let cityName = cityTimezone.replace("-", " ").split("/")[1];
+  if (cityTimezone === "current") {
+    cityTimezone = moment.tz.guess();
+  }
   let cityTime = moment().tz(cityTimezone);
+  let cityName = cityTimezone.replace("-", " ").split("/")[1];
   let cities = document.querySelector("#city");
   cities.innerHTML = `<div class="city first" id="vancouver">
           <div>
